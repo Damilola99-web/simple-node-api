caafc40ee92f5e93b2b8806fa0a7a7d9e10c3def
Damilola99-web <rasheedw622@gmail.com>
2022-10-26 09:57:37 +0100

    hey

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const userRoutes = require('./routes/users');
 const articleRouter = require('./routes/articles');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const server = express();
+server.use(cors())
 
 server.use(express.json())
 server.use('/api/users', userRoutes)
@@ -12,4 +14,4 @@ server.use('/api/articles', articleRouter)
 server.use('', (req, res) => {
 	res.status(404).json({ error: 'page not found' });
 });
-server.listen(3000);
+server.listen(3001);
